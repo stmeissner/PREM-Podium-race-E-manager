0673bf0e1c1b63b38df4c7215c4732779e9c6f2e
busterwolfeman <wolfgang@netracingeurope.org>
2017-12-25 11:56:25 +0100

    .

--- show_rules.php
+++ show_rules.php
@@ -1,7 +1,9 @@
 <? if(!defined("CONFIG")) exit();
+
 require_once("functions.php"); // import mysql function
 $link = mysqlconnect(); // call mysql function to get the link to the database
-$exe_rules = mysqli_query($link,"SELECT `rules` FROM rules_table WHERE `id` = 1 LIMIT 1");
+
+$exe_rules = mysqli_query($link,"SELECT rules FROM rules_table WHERE id = $_GET[id] LIMIT 1");
 list($rules) = mysqli_fetch_array($exe_rules);
 mysqli_free_result($exe_rules);
 if (!$rules) {
